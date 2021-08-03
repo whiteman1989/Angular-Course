9b2e2dc7a2a506d13c270f960a731a9c0b9833d8
AndreyGorban <Whiteman1989@gmail.com>
2021-08-03 23:59:41 +0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mu-first-app';
+  title = 'my app';
+  name = 'my name'
 }
