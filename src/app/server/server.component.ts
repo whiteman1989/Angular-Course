445f101676fb916987777a11afd37ede7ff335e6
AndreyGorban <Whiteman1989@gmail.com>
2021-08-09 00:02:34 +0300

    task26

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,14 @@ import { Component } from "@angular/core";
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
-    
+    serverId: number = 10;
+    serverStatus: string = 'offline'
+
+    getServerStatus(): string {
+        if(Math.random()<0.5) {
+            return this.serverStatus;
+        } else {
+            return "online"
+        }
+    }
 }
