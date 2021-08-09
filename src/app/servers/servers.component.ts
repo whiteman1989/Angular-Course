38df16c4a2b52a16625a78d896ed4e5d25b48f74
AndreyGorban <Whiteman1989@gmail.com>
2021-08-09 22:24:22 +0300

    task34

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -22,7 +22,7 @@ export class ServersComponent implements OnInit {
   }
 
   onServerCreate(): void {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = `Server ${this.serverName} was created!`;
   }
 
   onUpdateServerName(event: Event): void {
